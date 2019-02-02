903a61f84f5b47520fdc53bdd28548ad0576b7a9
misterlucky1982 <damonhill1996@tut.by>
2019-02-02 11:37:56 +0300

    commit C

--- src/main/java/by/game/proxi/IGameWorld.java
+++ src/main/java/by/game/proxi/IGameWorld.java
@@ -5,8 +5,8 @@ public interface IGameWorld {
 	void addTask(ITask task);
 	void addTask(Object actorId, ITask task);
 	void addRobot(IRobot robot);
-	IRobot getById(Object id);
-	IRobot someRobot();
 	IGameWorld setIGameActivityTracker(IGameActivityTracker activityTacker);
 	void startGame();
+	void setMaxTaskTime(long time);
+	void setTaskAmountThreshold(int threshold);
 }
